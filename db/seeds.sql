ba7036a16551a81f423bfb6dca2d150a1a95c23f
ganjioppa <Timkim@Tims-MacBook.local>
2018-12-13 23:10:51 -0800

    updating

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fish Burger');
